896dea0acdc0d299ac088cf73424d4452a34a43b
yongwl <yongwen6580@gmail.com>
2023-03-23 11:03:10 -0400

    Modified the type file.

---
+++ pkg/api/coinGecko_test.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"testing"
+)
+
+func TestList(t *testing.T) {
+	cg := CoinGecko{}
+	coins, err := cg.List()
+	if err != nil {
+		t.Errorf("error calling List(): %v", err)
+	}
+	if len(coins) == 0 {
+		t.Errorf("expected more than 0 coins, got %d", len(coins))
+	}
+}
+
+func TestGetTrending(t *testing.T) {
+	cg := CoinGecko{}
+	topCoin, err := cg.GetTrending()
+	if err != nil {
+		t.Errorf("error calling GetTrending(): %v", err)
+	}
+	if len(topCoin) == 0 {
+		t.Errorf("expected more than 0 top coins, got %d", len(topCoin))
+	}
+}
+
+func TestGetTokenPrice(t *testing.T) {
+	cg := CoinGecko{}
+	price, err := cg.GetTokenPrice("bitcoin")
+	if err != nil {
+		t.Errorf("error calling GetTokenPrice(%s): %v", "bitcoin", err)
+	}
+	if price.Usd <= 0 {
+		t.Errorf("expected price of %s to be greater than 0, got %.2f", "bitcoin", price.Usd)
+	}
+}
