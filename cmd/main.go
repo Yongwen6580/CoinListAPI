51ae802513b1510f88f26c357639381b1b78708b
yongwen6580 <yamafeiya@gmail.com>
2023-03-23 12:15:11 -0400

    Add unit test file

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/Yongwen6580/CoinListAPI/pkg/api"
 	"log"
+
+	"github.com/Yongwen6580/CoinListAPI/pkg/api"
 )
 
 func main() {
@@ -10,7 +11,7 @@ func main() {
 	cg := &api.CoinGecko{}
 	coins, err := cg.List()
 	for _, coin := range coins {
-		log.Printf("ID: %s, Name: %s, Symbol: %s\n", coin.Id, coin.Name, coin.Symbol)
+		log.Printf("ID: %v, Name: %v, Symbol: %v\n", coin.Id, coin.Name, coin.Symbol)
 	}
 	//call the get GetTrending() method
 	topCoin, err := cg.GetTrending()
@@ -18,13 +19,12 @@ func main() {
 		log.Fatal(err)
 	}
 	for _, topC := range topCoin {
-		log.Printf(" Coin ID: %d, Name: %s, Symbol: %s, price_btc: %9f, Market Cap Rank: %d\n", topC.CoinID, topC.Name, topC.Symbol, topC.Price, topC.MarketCapRank)
+		log.Printf(" Coin ID: %v, Name: %v, Symbol: %v, price_btc: %v, Market Cap Rank: %v\n", topC.CoinID, topC.Name, topC.Symbol, topC.Price, topC.MarketCapRank)
 	}
 	//call the get GetTokenPrice() method
-	name := "bitcoin"
-	price, err := cg.GetTokenPrice(name)
+	price, err := cg.GetTokenPrice("bitcoin")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Price of bitcoin: $%.2f\n", price.Usd)
+	log.Printf("Price of bitcoin: $%v\n", price.Usd)
 }
